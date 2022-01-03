c31ca539ae142c6c5de2072d9b26ae69ef893a57
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-03 11:49:22 +0100

    Navigation component style added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import "./NavigationStyle.css"
 
 class Navigation extends React.Component {
 
   render() {
     return (
-      <div>
-
-      </div>
+        <div className="NavButtonsContainer">
+          <Link className="NavButtons" to="/">Home</Link>
+          <Link className="NavButtons" to="/">placeholder</Link>
+        </div>
     );
   }
 }
