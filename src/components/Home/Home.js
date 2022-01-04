64351ab2e1785d308c87c91b95290af794f2803c
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-04 12:08:10 +0100

    Home page Container style class added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,48 @@
 import React from "react";
+import {SurveyCard} from "../SurveyCard/SurveyCard";
+import "./HomeStyle.css"
 
 class Home extends React.Component {
 
   render() {
     return (
-      <div>
-        Eddig meg jobb
+      <div className="SurveyCardContainer">
+        <SurveyCard surveyname="TestSurvey1"
+                    description="Test description1 asdf jj ajdh neafnr uanfnauf nuaewui aeanweu aefnaiwenf aena wenaweneu naenfeaue fndnfuae andfnei a ean ua naenan eu ae nu nena."
+                    surveycreator="Bob1"/>
+        <SurveyCard surveyname="TestSurvey2"
+                    description="Test description2"
+                    surveycreator="Bob2"/>
+        <SurveyCard surveyname="TestSurvey3"
+                    description="Test description3"
+                    surveycreator="Bob3"/>
+        <SurveyCard surveyname="TestSurvey1"
+                    description="Test description1"
+                    surveycreator="Bob1"/>
+        <SurveyCard surveyname="TestSurvey2"
+                    description="Test description2"
+                    surveycreator="Bob2"/>
+        <SurveyCard surveyname="TestSurvey3"
+                    description="Test description3"
+                    surveycreator="Bob3"/>
+        <SurveyCard surveyname="TestSurvey1"
+                    description="Test description1"
+                    surveycreator="Bob1"/>
+        <SurveyCard surveyname="TestSurvey2"
+                    description="Test description2dfhajskdhfakjsdhgjkafhgahdfjghad;fghadujfhgjadfhgjadhfgj;had;fjghadfgh;ujarhgujadfgjhadfghjiafhgujiadfhgiujadfhguiadfhgujadhfguadhfuighdaufighauidfhgidahfguihadfghd;ifghadi;fhgijadfhg;iadhfg;jiadhfg;jiadhfijghadijkfgha;djikfgha;ijdfhguae;rghuiadhguiadfghuiaerhguiafuighaui;erghauerhgauirhga"
+                    surveycreator="Bob2"/>
+        <SurveyCard surveyname="TestSurvey3"
+                    description="Test description3"
+                    surveycreator="Bob3"/>
+        <SurveyCard surveyname="TestSurvey1"
+                    description="Test description1"
+                    surveycreator="Bob1"/>
+        <SurveyCard surveyname="TestSurvey2"
+                    description="Test description2"
+                    surveycreator="Bob2"/>
+        <SurveyCard surveyname="TestSurvey3"
+                    description="Test description3"
+                    surveycreator="Bob3"/>
       </div>
     );
   }
