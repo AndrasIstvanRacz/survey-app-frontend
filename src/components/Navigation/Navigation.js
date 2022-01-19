fff77d0bcabd9952a0e71f03a62aa75dc4b0de2a
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-19 11:24:40 +0100

    Navbar hamburger menu fixed

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,33 +1,44 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./NavigationStyle.css"
+import Logo from "./icon/logo.png"
 
 
 class Navigation extends React.Component {
 
-  myFunction =()=> {
-    let x = document.getElementById("NavButtonsContainer");
-    if (x.className === "NavButtonsContainer") {
-      x.className += " responsive";
+  myFunction = () => {
+    let x = document.getElementById("navbarLinks");
+
+    if (x.className === "NavbarLinks") {
+      x.className += " active";
+      console.log(x.className)
     } else {
-      x.className = "NavButtonsContainer";
+      x.className = "NavbarLinks";
     }
   }
 
   render() {
     return (
-        <div className="NavButtonsContainer" id="NavButtonsContainer">
+      <div className="Navbar">
 
-          <img className="Logo" src={require("./icons/logo.png")} alt="description of image" height={32} width={32}/>
-
-
-          <Link className="NavButtons" to="/">Home</Link>
-          <Link className="NavButtons" to="/">placeholder</Link>
-          <Link className="LoginButton" to="/login">Log In</Link>
-          <a className="Menu" onClick={this.myFunction}>
-            <img className="MenuImage" src={require("./icons/dots.png")}/>
-          </a>
+        {/*<img className="Logo" src={require("./icon/logo.png")} alt="description of image" height={32} width={32}/>*/}
+        <div className="Title">
+          <img className="Logo" src={require("./icon/logo.png")} alt="survey app"/>
+          Survey App
         </div>
+        <div className="ToggleButton" onClick={this.myFunction}>
+          <span className="Bar"></span>
+          <span className="Bar"></span>
+          <span className="Bar"></span>
+        </div>
+        <div className="NavbarLinks" id="navbarLinks">
+          <Link to="/">Home</Link>
+          <Link to="/">Surveys</Link>
+          <Link to="/login">Log In</Link>
+        </div>
+
+
+      </div>
     );
   }
 }
