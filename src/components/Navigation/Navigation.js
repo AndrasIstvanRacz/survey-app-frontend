00703edf675430648b12b5dc92c72e8518985311
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-20 10:14:23 +0100

    cookieHandler created and implemented setCookie, getCookie and eraseCookie.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,49 +1,43 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./NavigationStyle.css"
-import Logo from "./icon/logo.png"
 
 
 class Navigation extends React.Component {
 
   DropDownOnClick = () => {
-    let x = document.getElementById("navbarLinks");
-
-    if (x.className === "NavbarLinks") {
-      x.className += " active";
+    let element = document.getElementById("navbarLinks");
+    if (element.className === "NavbarLinks") {
+      element.className += " active";
     } else {
-      x.className = "NavbarLinks";
+      element.className = "NavbarLinks";
     }
   }
 
   DropDownCloseOnClick = () => {
-    let x = document.getElementById("navbarLinks");
-    if (x.className === "NavbarLinks active") {
-      x.className = "NavbarLinks";
+    let element = document.getElementById("navbarLinks");
+    if (element.className === "NavbarLinks active") {
+      element.className = "NavbarLinks";
     }
   }
 
   render() {
     return (
       <div className="Navbar">
-
-        {/*<img className="Logo" src={require("./icon/logo.png")} alt="description of image" height={32} width={32}/>*/}
         <div className="Title">
           <img className="Logo" src={require("./icon/logo.png")} alt="survey app"/>
           Survey App
         </div>
         <div className="ToggleButton" onClick={this.DropDownOnClick}>
-          <span className="Bar"></span>
-          <span className="Bar"></span>
-          <span className="Bar"></span>
+          <span className="Bar"/>
+          <span className="Bar"/>
+          <span className="Bar"/>
         </div>
         <div className="NavbarLinks" id="navbarLinks">
           <Link to="/" onClick={this.DropDownCloseOnClick}>Home</Link>
           <Link to="/" onClick={this.DropDownCloseOnClick}>Surveys</Link>
           <Link to="/login" onClick={this.DropDownCloseOnClick}>Log In</Link>
         </div>
-
-
       </div>
     );
   }
